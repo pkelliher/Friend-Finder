166c30f1c8340472d0b502918cde88bce700efea
unknown <pkell@DESKTOP-ED2GH22.(none)>
2019-08-20 17:28:02 -0700

    New Changes

---
+++ FriendFinder/server.js
@@ -0,0 +1,27 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// create appliation/json parser
+var jsonParser = bodyParser.json();
+
+// parse application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/++json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type:'text/html' }))
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
